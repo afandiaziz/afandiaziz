01e05c24bf6f2d824b0bd342123ff475f01f2cd3
afandiaziz <afandiaziz46@gmail.com>
2022-07-12 09:49:46 +0700

    cv lang

--- pages/cv.js
+++ pages/cv.js
@@ -1,3 +1,10 @@
+import {useRouter} from "next/router";
+
 export default function Cv() {
-    return <iframe src="/cv.pdf"></iframe>;
+	const router = useRouter();
+	if (router.query.lang == "id") {
+		return <iframe src="/cv-id.pdf"></iframe>;
+	} else {
+		return <iframe src="/cv-eng.pdf"></iframe>;
+	}
 }
