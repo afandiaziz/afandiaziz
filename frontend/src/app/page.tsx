03c78b723022850b973486a8220a435613673ef3
afandiaziz <afandiaziz46@gmail.com>
2024-01-18 16:01:21 +0700

    chore: throw

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,13 +6,13 @@ async function getData() {
         let data: any = {};
         data.profile = await fetch(`${process.env.API_URI}/profile`, {
             // cache: 'no-cache'
-        }).then(res => res.json()).then(({ data }) => data).catch(err => { throw new Error('profile: ' + err) });
+        }).then(res => res.json()).then(({ data }) => data).catch(err => { throw new Error(`profile: ${process.env.API_URI}: ${err}`) });
         data.experiences = await fetch(`${process.env.API_URI}/experiences`, {
             // cache: 'no-cache'
-        }).then(res => res.json()).then(({ data }) => data.slice(0, 3)).catch(err => { throw new Error('experiences: ' + err) });
+        }).then(res => res.json()).then(({ data }) => data.slice(0, 3)).catch(err => { throw new Error(`experiences: ${process.env.API_URI}: ${err}`) });
         data.stacks = await fetch(`${process.env.API_URI}/stacks`, {
             // cache: 'no-cache'
-        }).then(res => res.json()).then(({ data }) => data).catch(err => { throw new Error('stacks: ' + err) });
+        }).then(res => res.json()).then(({ data }) => data).catch(err => { throw new Error(`stacks: ${process.env.API_URI}: ${err}`) });
         return data;
     } catch (error) {
         throw new Error("Fetching data: " + error);
